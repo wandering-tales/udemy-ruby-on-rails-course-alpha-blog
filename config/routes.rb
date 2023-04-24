caa52358b8764673cc5cce43f9d3c5b6d11754dc
Michele Cardone <michele.cardone@nozominetworks.com>
2023-04-24 17:44:44 +0200

    Add `destroy` action for articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
