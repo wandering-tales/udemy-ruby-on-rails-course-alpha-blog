adc861e449f9d31c9f113849c640b884ca48e7e0
Michele Cardone <michele.cardone@nozominetworks.com>
2023-06-02 18:36:52 +0200

    Add `User` reference to `Article`

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_02_160454) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_163421) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_02_160454) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
