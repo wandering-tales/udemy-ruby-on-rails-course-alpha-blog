dad0b99bba1a257fab000e73a65706e87e44397a
Michele Cardone <michele.cardone@nozominetworks.com>
2023-06-30 19:03:30 +0200

    Add pagination for both users and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@ class UsersController < ApplicationController
   before_action :load_user, only: [:show, :edit, :update]
 
   def show
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
